d9b8fc77ce1f171751a731ca28d32f4d9616e3fc
ryacha1983 <ryacha1983@gmail.com>
2024-01-25 07:55:43 +0300

    begin

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write($"Введите количество строк: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вы ввели:   {number}");
+string[] arraystring = new string[number];
+
+
+
+
+ for (int i = 0; i < number; i++) 
+{
+    Console.WriteLine("Введите строку: ");
+    string stringZero = Console.ReadLine();
+    // Console.WriteLine($"Вы ввели строку:   {stringZero}");
+    arraystring[i] = stringZero;
+} 
+Console.WriteLine($"Новый массив:");
+for (int i = 0; i < number; i++) 
+{ 
+    if (arraystring[i].Length <4)
+        Console.WriteLine($"   {arraystring[i]}");
+} 
