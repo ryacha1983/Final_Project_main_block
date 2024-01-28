0ecb4831ceeea1bf7a3b76732c00d0b3147d2502
ryacha1983 <ryacha1983@gmail.com>
2024-01-28 23:58:34 +0300

    диаграмма готова

--- Program.cs
+++ Program.cs
@@ -1,21 +1,30 @@
-﻿Console.Write($"Введите количество строк: ");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Вы ввели:   {number}");
-string[] arraystring = new string[number];
-
-
-
-
- for (int i = 0; i < number; i++) 
+﻿
+string [] CreateAarayString()
 {
-    Console.WriteLine("Введите строку: ");
-    string stringZero = Console.ReadLine();
-    // Console.WriteLine($"Вы ввели строку:   {stringZero}");
-    arraystring[i] = stringZero;
-} 
+    Console.Write($"Введите количество строк: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    string[] arraystring1 = new string[number];
+    for (int i = 0; i < number; i++) 
+    {
+        Console.WriteLine("Введите строку: ");
+        string stringZero = Console.ReadLine();
+        arraystring1[i] = stringZero;
+    } 
+    return arraystring1;
+}
+
+string[] arraystring = CreateAarayString();
+string[] NewArraystring = new string[arraystring.Length];
+int j=0;
+ 
 Console.WriteLine($"Новый массив:");
-for (int i = 0; i < number; i++) 
+for (int i = 0; i < arraystring.Length; i++) 
 { 
-    if (arraystring[i].Length <4)
-        Console.WriteLine($"   {arraystring[i]}");
+    if (arraystring[i].Length<4)
+    {
+        NewArraystring[j] = arraystring[i];
+        Console.WriteLine($"   {NewArraystring[j]}");
+        j+=1;
+    }
 } 
+
